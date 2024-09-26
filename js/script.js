0631ffdf90d844da6b5c45911b68cfa91c1f8c29
JFAquinoPando <pandolcik@gmail.com>
2024-09-25 21:27:04 -0400

    datos en el html

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 const principal = document.querySelector("main")
-const datosTexto = localStorage.getItem("poke-test")
-const datosJson = JSON.parse(datosTexto)
+/* const datosTexto = localStorage.getItem("poke-test")
+const datosJson = JSON.parse(datosTexto) */
+/* console.log(datosTexto, datosJson); */
 
-console.log(datosTexto, datosJson);
+fetch("datos.php").then(
+    peticion => peticion.json()
+).then(
+    informacion => {
+        const s = informacion.map(dato => {
+            return `<div class='poke'>
+                <h3>${dato.nombre}</h3>
+                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png">
+                <p>
+                Edad: ${dato.edad} años
+                </p>
+            </div>`
+        })
+        
+        console.log(s);
+        
+        principal.insertAdjacentHTML("afterbegin", s.join(""))
+    }
+)
 
-const s = datosJson.map(dato => {
-    return `<div class='poke'>
-        <h3>${dato.nombre}</h3>
-        <img src=${dato["imagen"]}>
-        <p>
-        ${dato.altura} m
-        <br/>
-        ${dato.peso} Kg
-        </p>
-    </div>`
-})
-
-console.log(s);
-
-principal.insertAdjacentHTML("afterbegin", s.join(""))
